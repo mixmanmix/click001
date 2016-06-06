116bb7889e5271e3d39b8aacdf41bc47d1301bca
MB Air <mbair@MBs-MacBook-Air.hitronhub.home>
2016-06-05 22:30:21 -0700

    A1

---
+++ ClickWash/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ClickWash
+//
+//  Created by MB Air on 2016-06-05.
+//  Copyright © 2016 deepfriedmobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+use_frameworks!
+target 'ClickWash' do
+pod 'Firebase'
+ 
+  # Pods for ClickWash
+
+end
